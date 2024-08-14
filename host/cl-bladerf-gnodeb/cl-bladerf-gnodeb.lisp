6de7470351e319c9df47b9fa753ef6ba947be2ce
Tichaona Kadzinga <tichaona@kadzinga.com>
2024-08-14 03:06:57 +0200

    Correct fix experimental code

--- host/cl-bladerf-gnodeb/cl-bladerf-gnodeb.lisp
+++ host/cl-bladerf-gnodeb/cl-bladerf-gnodeb.lisp
@@ -6,14 +6,14 @@
 
 (cl-bladerf::with-bladerf-device (dev1 "")
 
-  (setf (cffi:mem-aref *dword-bytes* :uint32 0 ) #b00000000000000000000000000001101) ; This is the number of Dwords in the transport block
+  (setf (cffi:mem-aref *dword-bytes* :uint32 0) #b00000000000000000000000000001101) ; This is the number of Dwords in the transport block
   (setf (cffi:mem-aref *dword-bytes* :uint32 1)  #b00000000000000000000000000001101)
   (setf (cffi:mem-aref *dword-bytes* :uint32 2)  #b00000000000000000000000000001101)
-  (setf (cffi:mem-aref *dword-bytes* :uint32 2)  #b00000000000000000000000000001101)
+  (setf (cffi:mem-aref *dword-bytes* :uint32 3)  #b00000000000000000000000000001101)
 
   (cl-bladerf::bladerf_sync_config dev1 :BLADERF_TX_X1  :BLADERF_FORMAT_PACKET_META  4096 4096 16 10000000)
   (cl-bladerf::bladerf_set_frequency dev1 (cl-bladerf::channel-tx 0) 918000000)
   (cl-bladerf::bladerf_set_sample_rate dev1 (cl-bladerf::channel-tx 0)  250000 (cffi:null-pointer))
   (cl-bladerf::bladerf_set_bandwidth dev1 (cl-bladerf::channel-tx 0) 1500000(cffi:null-pointer)  )
   (cl-bladerf::bladerf_enable_module dev1 (cl-bladerf::channel-tx 0) 1)
-  (cl-bladerf::bladerf_sync_tx dev1 *dword-bytes*  3   (cffi:null-pointer)  1000))
+  (cl-bladerf::bladerf_sync_tx dev1 *dword-bytes*  4   (cffi:null-pointer)  1000))
